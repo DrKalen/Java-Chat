fae677cb7bb3d9c69562481c44fa963f95fa3296
Kalen Hammann <kalen7@gmail.com>
2019-03-27 15:11:17 -0400

    updated client to receive as well as send

--- SimpleChatClientA.java
+++ SimpleChatClientA.java
@@ -1,31 +1,52 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
 public class SimpleChatClientA {
+  
+  JTextArea incoming;
   JTextField outgoing;
+  BufferedReader reader;
   PrintWriter writer;
   Socket sock;
 
   public void go() {
+
 	JFrame frame = new JFrame("Ludirously Simple Chat Client");
 	JPanel mainPanel = new JPanel();
+	incoming = new JTextArea(15,50);
+	incoming.setLineWrap(true);
+	incoming.setWrapStyleWord(true);
+	incoming.setEditable(false);
+	JScrollPane qScroller = new JScrollPane(incoming);
+	qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 	outgoing = new JTextField(20);
 	JButton sendButton = new JButton("Send");
 	sendButton.addActionListener(new SendButtonListener());
+
+	mainPanel.add(qScroller);
 	mainPanel.add(outgoing);
 	mainPanel.add(sendButton);
-	frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
 	setUpNetworking();
+
+	Thread readerThread = new Thread(new IncomingReader());
+	readerThread.start();
+
+	frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
 	frame.setSize(400,500);
 	frame.setVisible(true);
+
   } // close go
 
   private void setUpNetworking() {
     try{
 	sock = new Socket("127.0.0.1", 5000);
+	InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+	reader = new BufferedReader(streamReader);
 	writer = new PrintWriter(sock.getOutputStream());
 	System.out.println("networking established");
     } catch(IOException ex) {
@@ -46,6 +67,18 @@ public class SimpleChatClientA {
 	outgoing.requestFocus();
     }
   } // close SendButtonListener inner class
+
+  public class IncomingReader implements Runnable {
+    public void run() {
+	String message;
+	try {
+	  while ((message = reader.readLine()) != null) {
+		System.out.println("read " + message);
+		incoming.append(message + "\n");
+	  }  // close while
+ 	} catch(Exception ex) {ex.printStackTrace();}
+    } // close run
+  } // close IncomingReader	
 
   public static void main(String[] args) {
     new SimpleChatClientA().go();
